0ae6acd2cc089ad172c16865eb6470cb9ffd5075
Lev Borodin <levborodin@MacBook-Air-Lev.local>
2023-04-27 13:49:23 +0300

    [features] sass compiller added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <script setup>
+import AppTabs from "@/components/AppTabs/AppTabs.vue";
 
+const tabs = ['Main Info', 'Available Locations', 'Roles'];
 </script>
 
 <template>
-  <div>
-hi
+  <div class="app-wrapper">
+    <app-tabs :tabs="tabs" />
   </div>
 </template>
 
 <style lang="scss" scoped>
-
+.app-wrapper{
+  border: 1px solid red;
+}
 </style>
---
+++ src/components/AppTabs/AppTabs.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { toRefs } from 'vue'
+
+const props = defineProps({
+  tabs: {
+    type: Array,
+    required: true,
+    default: () => []
+  }
+})
+const { tabs } = toRefs(props)
+</script>
+
+<template>
+  <div>hello</div>
+</template>
+
+<style lang="scss" scoped></style>
